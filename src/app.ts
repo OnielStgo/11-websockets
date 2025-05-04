cd51f1a9f6e8e7af872c9d7f83764a037228c6b7
OnielStgo <onielst84@gmail.com>
2025-05-04 15:50:08 -0300

    configuração do servidor websocket e criação de um clinte

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -9,14 +9,34 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    console.log(`Mensagem do cliente: ${data}`);
+
+    const payload = JSON.stringify({
+      type: 'custom-message',
+      payload: data.toString().toUpperCase()
+    })
+
+    // ws.send(JSON.stringify(payload))
+
+    //enviar mensagem para todos os cliente (inclusive para o emisor)
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(payload, { binary: false });
+    //   }
+    // });
+
+    //enviar mensagem para todos os cliente (mas não para o emisor)
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
   });
 
-  ws.send('Olá desde o servidor');
+  ws.on('close', () => {
+    console.log('Cliente desconectado')
+  })
 
-  setInterval(() => {
-    console.log('Olá, tudo bem?')
-  }, 2000);
 });
 
-console.log('Server running on port localhost:3000')
+console.log('Server running on localhost:3000')
